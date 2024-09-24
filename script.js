fe424100bc14bc553efaf77015657a910a7fa716
szluh <142252543+szluh@users.noreply.github.com>
2024-09-24 09:07:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const url = ‘http://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json’
+
+async function visualizarinformacoesglobais() {
+	const res = await fetch(url)
+	const dados = await res.json()
+	console.log(dados);
+}
+
+visualizarinformacoesglobais()
+
